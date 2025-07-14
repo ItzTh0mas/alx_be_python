68ea5987dd161ead23439fd66a4b12759578dc4f
Lethabo <Letmasoga@gmail.com>
2025-07-14 08:05:27 +0000

    Add basic op script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print (f"Additon of 10 and 5 is {addition}")
+print(f"Subtraction of 10 and 5 is {subtraction}")
+print(f"Multiplication of 10 and 5 is {multiplication}")
+
