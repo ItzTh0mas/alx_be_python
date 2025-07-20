c820c1bb5515d8f3704ff5aefbcbebe05a1321e0
Lethabo <Letmasoga@gmail.com>
2025-07-20 18:52:51 +0000

    Fixed Reminder script

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,16 +5,16 @@ time_bound = input("Is it time-bound? (yes/no):").lower()
 match priority:
     case "high":
         if time_bound == "no":
-            print(f"{task} is high priority but not time bound. Keep in mind.")
+            print(f"Note: {task} is high priority but not time bound. Keep in mind.")
         elif time_bound == "yes":
-            print(f" WARNING: {task} is high priority and time bound. Conclude ASAP")
+            print(f" Reminder: {task} is high priority and time bound. Conclude ASAP")
     case "medium":
         if time_bound == "no":
-            print(f"{task} is medium priority and not time bound. Do not forget.")
+            print(f"Note: {task} is medium priority and not time bound. Do not forget.")
         elif time_bound == "yes":
-            print(f"{task} is time bound and medium priority. Advice start now")
+            print(f"Reminder: {task} is time bound and medium priority. Advice start now")
     case "low":
         if time_bound == "no":
-            print(f"{task} is low priority. Consider in your free time.")
+            print(f"Note: {task} is low priority. Consider in your free time.")
         elif time_bound  == "yes":
-            print(f"{task} is low priority yet time bound. Get it out the way now.")
+            print(f"Reminder: {task} is low priority yet time bound. Get it out the way now.")
