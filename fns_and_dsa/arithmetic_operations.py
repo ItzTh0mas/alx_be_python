284992c4c48291e6dfb6bed8a648f01f921594a7
Lethabo <Letmasoga@gmail.com>
2025-07-27 19:23:01 +0000

    Fix Arithmetic script

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,2 +1,15 @@
-def perform_operation:
-    num1 = float(input(""))
+def perform_operation(num1, num2, operation):
+    
+    if operation == 'add':
+        return float(num1 + num2)
+    elif operation == 'subtract':
+        return float(num1 - num2)
+    elif operation == 'multiply':
+        return float(num1 * num2)
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return float(num1 / num2)
+    else:
+        return "Error: Invalid operation"
+
